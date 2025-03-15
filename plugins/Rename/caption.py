b222e78d9b46321d700018a234aaf99f7fe0fffd
Amit <heroku467@gmail.com>
2025-03-15 13:40:40 +0530

    Update caption.py

--- plugins/Rename/caption.py
+++ plugins/Rename/caption.py
@@ -1,39 +1,28 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client, filters 
 from helper.database import db
-from config import RENAME_MODE
 
-@Client.on_message(filters.private & filters.command('set_caption'))
+@Client.on_message(filters.private & filters.command(['set_caption', "sc"]))
 async def add_caption(client, message):
-    if RENAME_MODE == False:
-        return 
-    caption = await client.ask(message.chat.id, "**__𝙶𝚒𝚟𝚎 𝚖𝚎 𝚊 𝚌𝚊𝚙𝚝𝚒𝚘𝚗 𝚝𝚘 𝚜𝚎𝚝.__\n\nAvailable Filling :-\n📂 File Name: `{filename}`\n\n💾 Size: `{filesize}`\n\n⏰ Duration: `{duration}`**")
-    await db.set_caption(message.from_user.id, caption=caption.text)
-    await message.reply_text("__**✅ 𝚈𝙾𝚄𝚁 𝙲𝙰𝙿𝚃𝙸𝙾𝙽 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝚅𝙴𝙳**__")
+    if len(message.command) == 1:
+       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+    caption = message.text.split(" ", 1)[1]
+    await jishubotz.set_caption(message.from_user.id, caption=caption)
+    await message.reply_text("**Your Caption Successfully Added ✅**")
 
-    
-@Client.on_message(filters.private & filters.command('del_caption'))
+
+@Client.on_message(filters.private & filters.command(['del_caption', "dc"]))
 async def delete_caption(client, message):
-    if RENAME_MODE == False:
-        return 
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await jishubotz.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("😔**Sorry ! No Caption found...**😔")
-    await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**Your Caption deleted successfully**✅️")
-                                       
-@Client.on_message(filters.private & filters.command('see_caption'))
+       return await message.reply_text("**You Don't Have Any Caption ❌**")
+    await jishubotz.set_caption(message.from_user.id, caption=None)
+    await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
+
+
+@Client.on_message(filters.private & filters.command(['see_caption', 'view_caption', "vc"]))
 async def see_caption(client, message):
-    if RENAME_MODE == False:
-        return 
-    caption = await db.get_caption(message.from_user.id)  
+    caption = await jishubotz.get_caption(message.from_user.id)  
     if caption:
-       await message.reply_text(f"**Your Caption:-**\n\n`{caption}`")
+       await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
     else:
-       await message.reply_text("😔**Sorry ! No Caption found...**😔")
-
-
-        
+       await message.reply_text("**You Don't Have Any Caption ❌**")
