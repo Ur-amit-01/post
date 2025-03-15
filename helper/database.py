f2ecf4e559b6b34f7d8b299c0c38974111699512
Amit <heroku467@gmail.com>
2025-03-15 14:29:42 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,68 +1,113 @@
+
 import motor.motor_asyncio
-from config import DB_URL, DB_NAME
+from config import Config
+from plugins.Rename.utils import send_log
 
 class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.users = self.db.user  # Collection for users
-        self.channels = self.db.channels  # Collection for channels
+        self.jishubotz = self._client[database_name]
+        self.col = self.jishubotz.user
 
     def new_user(self, id):
         return dict(
-            _id=int(id),
-            file_id=None,
-            caption=None
+            _id=int(id),                                   
+            file_id = None,
+            caption = None,
+            prefix = None,
+            suffix = None,
+            metadata = False,
+            metadata_code = "By :- @Madflix_Bots"
         )
 
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.users.insert_one(user)
+    async def add_user(self, b, m):
+        u = m.from_user
+        if not await self.is_user_exist(u.id):
+            user = self.new_user(u.id)
+            await self.col.insert_one(user)            
+            await send_log(b, u)
 
     async def is_user_exist(self, id):
-        user = await self.users.find_one({'_id': int(id)})
+        user = await self.col.find_one({'_id': int(id)})
         return bool(user)
 
     async def total_users_count(self):
-        count = await self.users.count_documents({})
+        count = await self.col.count_documents({})
         return count
 
     async def get_all_users(self):
-        all_users = self.users.find({})
+        all_users = self.col.find({})
         return all_users
 
     async def delete_user(self, user_id):
-        await self.users.delete_many({'_id': int(user_id)})
+        await self.col.delete_many({'_id': int(user_id)})
+    
+
+
+    #======================= Thumbnail ========================#
 
     async def set_thumbnail(self, id, file_id):
-        await self.users.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
     async def get_thumbnail(self, id):
-        user = await self.users.find_one({'_id': int(id)})
+        user = await self.col.find_one({'_id': int(id)})
         return user.get('file_id', None)
+    
+    
+
+    #======================= Caption ========================#
 
     async def set_caption(self, id, caption):
-        await self.users.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
 
     async def get_caption(self, id):
-        user = await self.users.find_one({'_id': int(id)})
+        user = await self.col.find_one({'_id': int(id)})
         return user.get('caption', None)
 
-    # Store the broadcast message ID
-    async def add_broadcast_message(self, user_id, message_id):
-        await self.users.update_one(
-            {"_id": user_id}, {"$set": {"last_broadcast_msg": message_id}}, upsert=True
-        )
 
-    # Retrieve the last broadcast message ID
-    async def get_broadcast_message(self, user_id):
-        user = await self.users.find_one({"_id": user_id})
-        return user.get("last_broadcast_msg") if user else None
 
-    # Delete the stored broadcast message ID
-    async def delete_broadcast_message(self, user_id):
-        await self.users.update_one({"_id": user_id}, {"$unset": {"last_broadcast_msg": ""}})
+    #======================= Prefix ========================#
+
+    async def set_prefix(self, id, prefix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})  
+        
+    async def get_prefix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('prefix', None)      
+    
+
+
+    #======================= Suffix ========================#
+
+    async def set_suffix(self, id, suffix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})  
+        
+    async def get_suffix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('suffix', None)
+
+
+
+    #======================= Metadata ========================#
+        
+    async def set_metadata(self, id, bool_meta):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
+        
+    async def get_metadata(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata', None)
+        
+        
+        
+    #======================= Metadata Code ========================#    
+        
+    async def set_metadata_code(self, id, metadata_code):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
+
+    async def get_metadata_code(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_code', None)   
 
 
     # =================== CHANNEL SYSTEM ===================
@@ -87,7 +132,13 @@ class Database:
     async def get_all_channels(self):
         """Retrieve all channel IDs as a list."""
         return [channel["_id"] async for channel in self.channels.find({})]
-
-
-# Initialize Database
+        
 db = Database(DB_URL, DB_NAME)
+
+
+
+
+# Stellar bots 
+# Don't Remove Credit 🥺
+# Telegram Channel @Stellar_bots
+# Developer @xDzod
