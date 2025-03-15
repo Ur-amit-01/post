37dfd4f0798d172301603823d2cd21e825228923
Amit <heroku467@gmail.com>
2025-03-16 01:11:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ from helper.database import db
 from pyrogram.errors import *
 import random
 from plugins.Fsub import auth_check
-from settings import back_to_settings
+from plugins.settings import back_to_settings
 
 # =====================================================================================
 @Client.on_message(filters.private & filters.command("start"))
