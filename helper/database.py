e5c0365f529039473099a3cb9e8cdcb478280c78
Amit <heroku467@gmail.com>
2025-03-20 18:32:41 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -5,130 +5,40 @@ class Database:
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
-        self.col = self.db.user  # Collection for users
         self.channels = self.db.channels  # Collection for channels
-        self.formatting = self.db.formatting  # Collection for formatting templates
-        self.admins = self.db.admins  # Collection for admins
-        self.posts = self.db.posts  # Collection for storing posts and their message IDs
-        self.latest_posts = self.db.latest_posts  # Collection for storing latest post
+        self.posts = self.db.posts  # Collection for posts
 
-    #============ User System ============#
-    def new_user(self, id):
-        return dict(
-            _id=int(id),
-            file_id=None,
-            caption=None,
-            prefix=None,
-            suffix=None,
-            metadata=False,
-            metadata_code="By :- @Madflix_Bots"
-        )
-
-    async def add_user(self, id):
-        if not await self.is_user_exist(id):
-            user = self.new_user(id)
-            await self.col.insert_one(user)
-
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return bool(user)
-
-    async def total_users_count(self):
-        return await self.col.count_documents({})
-
-    async def get_all_users(self):
-        return self.col.find({})
-
-    async def delete_user(self, user_id):
-        await self.col.delete_one({'_id': int(user_id)})
-
-    # Thumbnail
-    async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
-
-    async def get_thumbnail(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
-
-    # Caption
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
-
-    async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
-
-    #============ Channel System ============#
-    async def add_channel(self, channel_id, channel_name):
-        """Add a new channel to the database."""
-        existing_channel = await self.channels.find_one({"_id": int(channel_id)})
-        if not existing_channel:
-            await self.channels.insert_one({"_id": int(channel_id), "name": channel_name})
+    # Channel System
+    async def add_channel(self, channel_id, channel_name=None):
+        channel_id = int(channel_id)
+        if not await self.is_channel_exist(channel_id):
+            await self.channels.insert_one({"_id": channel_id, "name": channel_name})
             return True
         return False
 
-    async def is_channel_exist(self, channel_id):
-        """Check if a channel exists in the database."""
-        return bool(await self.channels.find_one({"_id": int(channel_id)}))
-
     async def delete_channel(self, channel_id):
-        """Remove a channel from the database."""
         await self.channels.delete_one({"_id": int(channel_id)})
 
+    async def is_channel_exist(self, channel_id):
+        return await self.channels.find_one({"_id": int(channel_id)}) is not None
+
     async def get_all_channels(self):
-        """Retrieve all connected channels."""
-        return await self.channels.find().to_list(None)
+        return [channel async for channel in self.channels.find({})]
 
-    #============ Post System ============#
-    async def save_post_messages(self, post_id, sent_messages):
-        """Save sent message IDs of a post for tracking and deletion."""
+    # Post System
+    async def save_latest_post(self, messages):
         await self.posts.update_one(
-            {"_id": post_id},
-            {"$set": {"messages": sent_messages}},
-            upsert=True
-        )
-
-    async def get_post_messages(self, post_id):
-        """Retrieve message IDs of a post."""
-        post_data = await self.posts.find_one({"_id": post_id})
-        return post_data["messages"] if post_data else {}
-
-    async def delete_post_messages(self, post_id):
-        """Delete post tracking data from the database."""
-        await self.posts.delete_one({"_id": post_id})
-
-    #============ Latest Post System ============#
-    async def save_latest_post(self, sent_messages):
-        """Save the latest post sent to channels."""
-        await self.latest_posts.update_one(
             {"_id": "latest_post"},
-            {"$set": {"messages": sent_messages}},
+            {"$set": {"messages": messages}},
             upsert=True
         )
 
     async def get_latest_post(self):
-        """Retrieve the latest post details."""
-        post_data = await self.latest_posts.find_one({"_id": "latest_post"})
-        return post_data["messages"] if post_data else {}
+        post = await self.posts.find_one({"_id": "latest_post"})
+        return post.get("messages", {}) if post else {}
 
     async def delete_latest_post(self):
-        """Delete the latest post from the database."""
-        await self.latest_posts.delete_one({"_id": "latest_post"})
-
-    #============ Formatting System ============#
-    async def save_formatting(self, channel_id, formatting_text):
-        """Save or update formatting text for a channel."""
-        await self.formatting.update_one(
-            {"_id": int(channel_id)},
-            {"$set": {"formatting_text": formatting_text}},
-            upsert=True
-        )
-
-    async def get_formatting(self, channel_id):
-        """Retrieve formatting text for a channel."""
-        result = await self.formatting.find_one({"_id": int(channel_id)})
-        return result.get("formatting_text") if result else None
-
+        await self.posts.delete_one({"_id": "latest_post"})
 
 # Initialize the database
 db = Database(DB_URL, DB_NAME)
