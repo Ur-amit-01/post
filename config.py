764b0552e7982fe338a9479e70377195a3efc555
Amit <heroku467@gmail.com>
2025-03-21 12:09:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7821967646:AAFHUS91204U6P6xqnBOdAefk42agRWzTc0") 
 
 DB_NAME = os.environ.get("DB_NAME","SAT_manager")     
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6803505727').split()]
 PORT = os.environ.get("PORT", "8080")
